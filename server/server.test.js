57f9f178a24377929f3dfff304d5a591c7a40fc7
nickpetronino <petroninon+coding@gmail.com>
2024-04-12 11:15:56 -0400

    UPDATE server.test.js

--- server/server.test.js
+++ server/server.test.js
@@ -1,26 +1,21 @@
 const request = require('supertest');
-const { app, albumSearch, getToken } = require('./server'); // Adjust the path as needed
+const { app, albumSearch, getToken } = require('./server');
 
-describe('GET /status', () => {
-
+describe('GET /status tests', () => {
     it('responds with status 200', async () => {
         const response = await request(app).get('/status');
         expect(response.status).toBe(200);
-    });
-
+    })
     it('responds with JSON containing the status message', async () => {
         const response = await request(app).get('/status');
         expect(response.body).toEqual({ "Status": "Running" });
     });
-
 });
 
-describe('getToken', () => {
+describe('getToken tests', () => {
     it('should fetch access token correctly', async () => {
-        // Call the actual getToken function which makes a real API call
         const response = await getToken();
 
-        // Verify that the response is an object with the expected properties
         expect(response).toEqual(expect.objectContaining({
             access_token: expect.any(String),
             token_type: 'Bearer',
@@ -29,7 +24,7 @@ describe('getToken', () => {
     });
 });
 
-describe('Album Search tests', () => {
+describe('albumSearch tests ', () => {
   it('should return albums matching the search string', async () => {
     const searchString = 'parachutes';
     const result = await albumSearch(searchString);
@@ -53,15 +48,14 @@ describe('Album Search tests', () => {
   });
 
   it('should handle a single space', async () => {
-    const searchString = ' '; // Single space which may cause an error
+    const searchString = ' ';
     const result = await albumSearch(searchString);
     expect(result).toBe("400 Bad request.");
   });
 });
 
-describe('POST /search', () => {
+describe('POST /search tests', () => {
     it('should return albums matching the search string from Spotify API', async () => {
-        // Define the search string
         const searchString = 'parachutes';
 
         // Send a mock request to the /search route
@@ -69,16 +63,12 @@ describe('POST /search', () => {
             .post('/search')
             .send({ searchString });
 
-        // Expect the response to have a status of 200
         expect(res.status).toBe(200);
-
-        // Validate the structure and content of the response body
         expect(res.body).toBeDefined();
         expect(res.body.length).toBeGreaterThan(0);
         expect(res.body[0]).toHaveProperty('id');
         expect(res.body[0]).toHaveProperty('name');
         expect(res.body[0]).toHaveProperty('total_tracks');
         expect(res.body[0]).toHaveProperty('artists');
-        // Add more specific assertions as needed based on the structure of the response
     });
 });
