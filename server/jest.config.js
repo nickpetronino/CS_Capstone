15ae8547f1bb2cd69cd96f1ed1fb54188eb85164
nickpetronino <petroninon+coding@gmail.com>
2024-04-25 16:55:53 -0400

    1.3 Finished, 100% Coverage, Full Useability.

--- server/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testEnvironment: 'node', // Use 'node' for testing in a Node.js environment
-    detectOpenHandles: true,
-    verbose: true,
-    coverageDirectory: 'coverage',
-  };
-  
