bc33a3122df2a85457d350003ada06771d495bd8
nickpetronino <petroninon+coding@gmail.com>
2024-04-26 12:26:42 -0400

    Finalize Client Side for PR3

--- you-review/src/components/AlbumList.js
+++ you-review/src/components/AlbumList.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Badge from 'react-bootstrap/Badge';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ListGroup from "react-bootstrap/ListGroup";
+import Badge from "react-bootstrap/Badge";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 /**
  * AlbumList component displays a list of albums with basic information.
@@ -14,33 +14,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  * @returns {JSX.Element} JSX.Element
  */
 const AlbumList = (props) => {
-    const { albums = [], noResults } = props;
+  const { albums = [], noResults } = props;
 
-    return noResults ? <h3>No results found!</h3> : (
-        <ListGroup as="ol">
-    {albums.map((album, index) => (
+  return noResults ? (
+    <h3>No results found!</h3>
+  ) : (
+    <ListGroup as="ol">
+      {albums.map((album, index) => (
         <Link to={`album/${index}`} className="list-group-item-action">
-            <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-            >
-                <div>
-                    <img src={album.images} alt={`${album.name} Album Cover`} height='100px' width='100px'/>
-                </div>
-                <div className="ms-2 me-auto" align="left">
-                    <div className="fw-bold">{album.name}</div>
-                    <div className="fw-normal"> {album.artists} </div>
-                    <div className="fw-light"> {album.release_date} </div>
-                </div>
-                <Badge bg="primary" pill>
-                    Tracks: {album.total_tracks}
-                </Badge>
-            </ListGroup.Item>
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+          >
+            <div>
+              <img
+                src={album.images}
+                alt={`${album.name} Album Cover`}
+                height="100px"
+                width="100px"
+              />
+            </div>
+            <div className="ms-2 me-auto" align="left">
+              <div className="fw-bold">{album.name}</div>
+              <div className="fw-normal"> {album.artists} </div>
+              <div className="fw-light"> {album.release_date} </div>
+            </div>
+            <Badge bg="primary" pill>
+              Tracks: {album.total_tracks}
+            </Badge>
+          </ListGroup.Item>
         </Link>
-    ))}
-</ListGroup>
+      ))}
+    </ListGroup>
+  );
+};
 
-    )
-}
-
-export default  AlbumList
+export default AlbumList;
